51cec51f6447c1f9da270f62940492c7705b70cb
Joao <bcastillo-2022474@kinal.edu.gt>
2022-12-06 21:22:11 -0600

    fixing some minus things

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -4,7 +4,7 @@ export const renderFigure = (box, isCircle) => {
   // if (box.querySelector(".circle") || box.querySelector(".cross-stripe")) return;
   // const markup = `<div class="${isCircle ? "cross-stripe" : "circle"}"></div>`;
   const markup = `<i class="las ${
-    !isCircle ? "la-fire" : "la-dragon"
+    !isCircle ? "la-paw" : "la-fire"
   } figure player-${!isCircle ? 2 : 1}__figure"></i>`;
   box.insertAdjacentHTML("afterbegin", markup);
   box.dataset.value = isCircle ? "X" : "O";
